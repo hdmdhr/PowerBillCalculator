a3ed5b25c0cc333a1f7a07558ba278e0ca8810ed
DongMing Hu <821312@ACDM.DS.SAIT.CA>
2019-02-21 08:05:12 -0700

    Extract calculation into methods; Iterate all textbox and reset; Hide and show when click on radio button.

--- PowerBillCalculator/frmCalculator.Designer.cs
+++ PowerBillCalculator/frmCalculator.Designer.cs
@@ -36,7 +36,7 @@
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grpForUsage = new System.Windows.Forms.GroupBox();
             this.txtUsage = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.grpForIndustrial = new System.Windows.Forms.GroupBox();
@@ -46,13 +46,15 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txtTotal = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtPeakCharge = new System.Windows.Forms.TextBox();
+            this.grpForIndusAmt = new System.Windows.Forms.GroupBox();
             this.txtOPCharge = new System.Windows.Forms.TextBox();
+            this.txtPeakCharge = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.grpForUsage.SuspendLayout();
             this.grpForIndustrial.SuspendLayout();
+            this.grpForIndusAmt.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -99,22 +101,26 @@
             this.radCommercial.TabIndex = 0;
             this.radCommercial.Text = "Commercial";
             this.radCommercial.UseVisualStyleBackColor = true;
+            this.radCommercial.CheckedChanged += new System.EventHandler(this.radCommercial_CheckedChanged);
             // 
             // radResidential
             // 
             this.radResidential.AutoSize = true;
+            this.radResidential.Checked = true;
             this.radResidential.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radResidential.Location = new System.Drawing.Point(0, 33);
             this.radResidential.Name = "radResidential";
             this.radResidential.Size = new System.Drawing.Size(132, 32);
             this.radResidential.TabIndex = 0;
+            this.radResidential.TabStop = true;
             this.radResidential.Text = "Residential";
             this.radResidential.UseVisualStyleBackColor = true;
+            this.radResidential.CheckedChanged += new System.EventHandler(this.radResidential_CheckedChanged);
             // 
             // btnCalculate
             // 
             this.btnCalculate.AutoSize = true;
-            this.btnCalculate.Location = new System.Drawing.Point(62, 646);
+            this.btnCalculate.Location = new System.Drawing.Point(506, 641);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(116, 38);
             this.btnCalculate.TabIndex = 2;
@@ -132,14 +138,14 @@
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(59, 38);
             this.btnExit.TabIndex = 2;
-            this.btnExit.Text = "Exit";
+            this.btnExit.Text = "&Exit";
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnReset
             // 
             this.btnReset.AutoSize = true;
-            this.btnReset.Location = new System.Drawing.Point(673, 646);
+            this.btnReset.Location = new System.Drawing.Point(229, 641);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(113, 38);
             this.btnReset.TabIndex = 2;
@@ -147,16 +153,17 @@
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // groupBox2
+            // grpForUsage
             // 
-            this.groupBox2.AutoSize = true;
-            this.groupBox2.Controls.Add(this.txtUsage);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(62, 158);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(724, 112);
-            this.groupBox2.TabIndex = 3;
-            this.groupBox2.TabStop = false;
+            this.grpForUsage.AutoSize = true;
+            this.grpForUsage.Controls.Add(this.txtUsage);
+            this.grpForUsage.Controls.Add(this.label2);
+            this.grpForUsage.Location = new System.Drawing.Point(62, 137);
+            this.grpForUsage.Margin = new System.Windows.Forms.Padding(0);
+            this.grpForUsage.Name = "grpForUsage";
+            this.grpForUsage.Size = new System.Drawing.Size(724, 112);
+            this.grpForUsage.TabIndex = 3;
+            this.grpForUsage.TabStop = false;
             // 
             // txtUsage
             // 
@@ -164,7 +171,7 @@
             this.txtUsage.Location = new System.Drawing.Point(509, 44);
             this.txtUsage.Name = "txtUsage";
             this.txtUsage.Size = new System.Drawing.Size(125, 35);
-            this.txtUsage.TabIndex = 1;
+            this.txtUsage.TabIndex = 0;
             // 
             // label2
             // 
@@ -178,17 +185,13 @@
             // grpForIndustrial
             // 
             this.grpForIndustrial.AutoSize = true;
-            this.grpForIndustrial.Controls.Add(this.txtOPCharge);
-            this.grpForIndustrial.Controls.Add(this.txtPeakCharge);
             this.grpForIndustrial.Controls.Add(this.txtOPUsage);
             this.grpForIndustrial.Controls.Add(this.txtPeakUsage);
-            this.grpForIndustrial.Controls.Add(this.label7);
-            this.grpForIndustrial.Controls.Add(this.label6);
             this.grpForIndustrial.Controls.Add(this.label4);
             this.grpForIndustrial.Controls.Add(this.label3);
-            this.grpForIndustrial.Location = new System.Drawing.Point(62, 286);
+            this.grpForIndustrial.Location = new System.Drawing.Point(62, 252);
             this.grpForIndustrial.Name = "grpForIndustrial";
-            this.grpForIndustrial.Size = new System.Drawing.Size(724, 272);
+            this.grpForIndustrial.Size = new System.Drawing.Size(724, 154);
             this.grpForIndustrial.TabIndex = 3;
             this.grpForIndustrial.TabStop = false;
             this.grpForIndustrial.Visible = false;
@@ -199,7 +202,7 @@
             this.txtOPUsage.Location = new System.Drawing.Point(509, 86);
             this.txtOPUsage.Name = "txtOPUsage";
             this.txtOPUsage.Size = new System.Drawing.Size(125, 35);
-            this.txtOPUsage.TabIndex = 1;
+            this.txtOPUsage.TabIndex = 2;
             // 
             // txtPeakUsage
             // 
@@ -230,9 +233,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(263, 586);
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(248, 586);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(216, 28);
+            this.label5.Size = new System.Drawing.Size(229, 28);
             this.label5.TabIndex = 0;
             this.label5.Text = "Total Amount (CAD)";
             // 
@@ -245,50 +249,66 @@
             this.txtTotal.Size = new System.Drawing.Size(125, 35);
             this.txtTotal.TabIndex = 1;
             // 
-            // label6
+            // grpForIndusAmt
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(164, 148);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(253, 28);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Peak hour charge (CAD)";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(127, 207);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(290, 28);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "Off peak hour charge (CAD)";
-            // 
-            // txtPeakCharge
-            // 
-            this.txtPeakCharge.Enabled = false;
-            this.txtPeakCharge.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPeakCharge.Location = new System.Drawing.Point(509, 145);
-            this.txtPeakCharge.Name = "txtPeakCharge";
-            this.txtPeakCharge.Size = new System.Drawing.Size(125, 35);
-            this.txtPeakCharge.TabIndex = 1;
+            this.grpForIndusAmt.Controls.Add(this.txtOPCharge);
+            this.grpForIndusAmt.Controls.Add(this.txtPeakCharge);
+            this.grpForIndusAmt.Controls.Add(this.label7);
+            this.grpForIndusAmt.Controls.Add(this.label6);
+            this.grpForIndusAmt.Location = new System.Drawing.Point(62, 412);
+            this.grpForIndusAmt.Name = "grpForIndusAmt";
+            this.grpForIndusAmt.Size = new System.Drawing.Size(724, 150);
+            this.grpForIndusAmt.TabIndex = 5;
+            this.grpForIndusAmt.TabStop = false;
+            this.grpForIndusAmt.Visible = false;
             // 
             // txtOPCharge
             // 
             this.txtOPCharge.Enabled = false;
             this.txtOPCharge.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtOPCharge.Location = new System.Drawing.Point(509, 204);
+            this.txtOPCharge.Location = new System.Drawing.Point(507, 91);
             this.txtOPCharge.Name = "txtOPCharge";
             this.txtOPCharge.Size = new System.Drawing.Size(125, 35);
             this.txtOPCharge.TabIndex = 1;
             // 
+            // txtPeakCharge
+            // 
+            this.txtPeakCharge.Enabled = false;
+            this.txtPeakCharge.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeakCharge.Location = new System.Drawing.Point(507, 32);
+            this.txtPeakCharge.Name = "txtPeakCharge";
+            this.txtPeakCharge.Size = new System.Drawing.Size(125, 35);
+            this.txtPeakCharge.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(125, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(290, 28);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Off peak hour charge (CAD)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(253, 28);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Peak hour charge (CAD)";
+            // 
             // frmCalculator
             // 
+            this.AcceptButton = this.btnCalculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 703);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(856, 691);
+            this.Controls.Add(this.grpForIndusAmt);
             this.Controls.Add(this.txtTotal);
             this.Controls.Add(this.grpForIndustrial);
-            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.grpForUsage);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnExit);
@@ -301,10 +321,12 @@
             this.Text = "Power Bill Calculator";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.grpForUsage.ResumeLayout(false);
+            this.grpForUsage.PerformLayout();
             this.grpForIndustrial.ResumeLayout(false);
             this.grpForIndustrial.PerformLayout();
+            this.grpForIndusAmt.ResumeLayout(false);
+            this.grpForIndusAmt.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -320,7 +342,7 @@
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnReset;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox grpForUsage;
         private System.Windows.Forms.TextBox txtUsage;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox grpForIndustrial;
@@ -330,10 +352,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtTotal;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox grpForIndusAmt;
         private System.Windows.Forms.TextBox txtOPCharge;
         private System.Windows.Forms.TextBox txtPeakCharge;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- PowerBillCalculator/frmCalculator.cs
+++ PowerBillCalculator/frmCalculator.cs
@@ -15,7 +15,7 @@ namespace PowerBillCalculator
         const double RATE_RESIDENTIAL = 0.052;
         const double RATE_COMMERCIAL = 0.045;
         const double PH_INDUSTRIAL = 0.065;
-        const double OF_INDUSTRIAL = 0.028;
+        const double OP_INDUSTRIAL = 0.028;
         double totalAmt;
         public frmCalculator()
         {
@@ -29,35 +29,120 @@ namespace PowerBillCalculator
 
         private void btnReset_Click(object sender, EventArgs e)
         {
-            // todo: google how to grab all rad and txt, iterate through them, clear all contents
+            // grab all rad and txt, iterate through them, clear all contents
+            foreach (Control c in Controls)
+            {
+                foreach (Control childc in c.Controls)
+                {
+                    if (childc is TextBox)
+                    {
+                        ((TextBox)childc).Text = "";
+                    }
+                }
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = "";
+                }
+            }
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            if (radResidential.Checked)
+            grpForIndusAmt.Visible = false;  // hide group for indust
+
+            // extract usage from textbox, todo: validation
+            double usage = 0;
+            if (!radIndustrial.Checked)
             {
-                // use residential formula
-                totalAmt = 6 + Convert.ToDouble(txtUsage.Text) * RATE_RESIDENTIAL;
-                txtTotal.Text = totalAmt.ToString("c");
-            } else if (radCommercial.Checked)
-            {
-                // use commercial formula
-            } else
-            {
-                // use industrial formula
+                if (IsInteger(txtUsage.Text))
+                    usage = Convert.ToDouble(txtUsage.Text);
+                else
+                    MessageBox.Show("Hours needs to be whole number");
             }
+            // todo: validate to make sure radio is checked
+            if (radResidential.Checked)
+                totalAmt = calcResi(usage);  // use residential method   
+            else if (radCommercial.Checked)
+                totalAmt = calcComm(usage); // use commercial method
+            else
+            {
+                // todo: validation
+                double peakUsage = Convert.ToDouble(txtPeakUsage.Text);
+                double opUsage = Convert.ToDouble(txtOPUsage.Text);
+                double peakAmt = 0, opAmt = 0;
+                totalAmt = calcIndus(peakUsage,opUsage,out peakAmt,out opAmt);  // use industrial method
+                // output separate use charges
+                grpForIndusAmt.Visible = true;
+                txtOPCharge.Text = opAmt.ToString("c");
+                txtPeakCharge.Text = peakAmt.ToString("c");
+            }
+            txtTotal.Text = totalAmt.ToString("c");
         }
+
+        //--------------Methods Area--------------------//
+        private double calcResi(double usage)
+        {
+            return 6 + usage * RATE_RESIDENTIAL;
+        }
+
+        private double calcComm(double usage)
+        {
+            if (usage <= 1000)
+                totalAmt = 60;
+            else
+                totalAmt = 60 + (usage - 1000) * RATE_COMMERCIAL;
+            return totalAmt;
+        }
+
+        private double calcIndus(double peakUse, double opUse, out double pAmt, out double opAmt)
+        {
+            if (peakUse <= 1000)
+                pAmt = 76;
+            else
+                pAmt = 76 + (peakUse - 1000) * PH_INDUSTRIAL;
+
+            if (opUse <= 1000)
+                opAmt = 40;
+            else
+                opAmt = 40 + (opUse - 1000) * OP_INDUSTRIAL;
+
+            return pAmt + opAmt;
+        }
+
+        public static bool IsInteger(string input)
+        {
+            int myNum;
+            if (Int32.TryParse(input, out myNum))
+                return true;
+            else
+                return false;
+        }
+
+        //------------------------------------------------//
 
         private void radIndustrial_CheckedChanged(object sender, EventArgs e)
         {
+            // hide and show corresponding groupbox
             RadioButton rad = (RadioButton)sender;
             if (rad.Checked)
             {
                 grpForIndustrial.Visible = true;
+                grpForUsage.Visible = false;
             } else
             {
                 grpForIndustrial.Visible = false;
+                grpForUsage.Visible = true;
             }
+        }
+
+        private void radCommercial_CheckedChanged(object sender, EventArgs e)
+        {
+            btnReset_Click(sender, e);
+        }
+
+        private void radResidential_CheckedChanged(object sender, EventArgs e)
+        {
+            btnReset_Click(sender, e);
         }
     }
 }
